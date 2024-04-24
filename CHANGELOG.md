203aa59859c4315af9e75386b9c751d9e8d7f0b5
Didem Yakici <didematilla@gmail.com>
2024-04-24 13:37:40 +0100

    Update to v1.0.13
    Update silentauth-sdk-ios to v1.0.10

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 # Changelog
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+### 1.0.13 (2024-04-24)
+### Changes
+* Bump silentauth-ios-sdk dependency to v1.0.10. (PrivacyInfo added)
 ### 1.0.12 (2024-03-14)
 ### Changes
 * Bump silentauth-android-sdk dependency to v1.0.8.
--- example/ios/Pods/Target Support Files/Pods-SilentAuthSdkReactNativeExample/Pods-SilentAuthSdkReactNativeExample-resources.sh
+++ example/ios/Pods/Target Support Files/Pods-SilentAuthSdkReactNativeExample/Pods-SilentAuthSdkReactNativeExample-resources.sh
@@ -98,9 +98,11 @@ EOM
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/React-Core/AccessibilityResources.bundle"
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/silentauth-sdk-ios/silentauth-sdk-ios.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/React-Core/AccessibilityResources.bundle"
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/silentauth-sdk-ios/silentauth-sdk-ios.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
